4f3f1f31cea1d8fbca516696d8c44e7279b9efdb
Vladyslav Shepilov <shepsterya@gmail.com>
2024-04-28 18:15:31 +0300

    add extended .gitignore

--- theatre/settings.py
+++ theatre/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "theatre-service",
+    "theatre_service",
 ]
 
 MIDDLEWARE = [
