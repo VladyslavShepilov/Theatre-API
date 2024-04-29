68c4dbf49b70f255deea4db269b50028d8bff4f9
Vladyslav Shepilov <shepsterya@gmail.com>
2024-04-29 15:19:53 +0300

    fix .gitignore

--- theatre_service/models.py
+++ theatre_service/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Play(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
