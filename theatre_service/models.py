972e242b9f391bd79ded923749d5ffad3305082c
Vladyslav Shepilov <shepsterya@gmail.com>
2024-04-30 00:08:23 +0300

    extend models Play model to show actors and genres

--- theatre_service/models.py
+++ theatre_service/models.py
@@ -6,6 +6,10 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class Genre(models.Model):
     name = models.CharField(max_length=255)
@@ -14,6 +18,8 @@ class Genre(models.Model):
 class Play(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    actors = models.ManyToManyField(Actor)
+    genres = models.ManyToManyField(Genre)
 
 
 class TheatreHall(models.Model):
