fac46d99ed5591349b9b1a05a829f515930becda
Vladyslav Shepilov <shepsterya@gmail.com>
2024-05-01 16:06:24 +0300

    add endpoints for Genres and Actors

---
+++ theatre_service/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from theatre_service.views import (
+    ActorViewSet,
+    GenreViewSet,
+    # PlayViewSet
+)
+
+app_name = "theatre-api"
+
+router = routers.DefaultRouter()
+router.register("actors", ActorViewSet)
+router.register("genres", GenreViewSet)
+# router.register("plays", PlayViewSet)
+
+
+urlpatterns = [path("", include(router.urls))]
--- theatre_service/views.py
+++ theatre_service/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework import viewsets, mixins
 
-# Create your views here.
+from theatre_service.models import (
+    Actor,
+    Genre,
+    Play,
+    TheatreHall,
+    Performance,
+    Ticket,
+    Reservation,
+)
+from theatre_service.serializers import (
+    ActorSerializer,
+    GenreSerializer,
+    PlayListSerializer,
+    PlayDetailSerializer,
+)
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class PlayViewSet(viewsets.ModelViewSet):
+    ...
