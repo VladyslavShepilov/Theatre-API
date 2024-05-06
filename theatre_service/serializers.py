119a30afe679ea173d9e96be22bf6146cde0968f
Vladyslav Shepilov <shepsterya@gmail.com>
2024-05-06 22:27:01 +0300

    add user serializer

--- theatre_service/serializers.py
+++ theatre_service/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db import transaction
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
@@ -166,3 +167,15 @@ class ReservationDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reservation
         fields = ("id", "tickets")
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = get_user_model()
+        fields = ("id", "username", "email", "password", "is_staff")
+        read_only_fields = ("id", "is_staff")
+        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
+
+    def create(self, validated_data):
+        return get_user_model().objects.create_user(**validated_data)
