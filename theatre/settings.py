8a870f4ea636f95456bd6f42769348604a0598ed
Vladyslav Shepilov <shepsterya@gmail.com>
2024-05-08 01:41:06 +0300

    switch to JWT authentication

--- theatre/settings.py
+++ theatre/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+from datetime import timedelta
 from pathlib import Path
 
 
@@ -139,6 +140,12 @@ REST_FRAMEWORK = {
         "theatre.permissions.IsAdminOrIfAuthenticatedReadOnly",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ]
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": False,
 }
--- theatre/urls.py
+++ theatre/urls.py
@@ -1,14 +1,20 @@
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 from theatre import settings
-from theatre.views import CreateUserView, CreateTokenView, ManageUserView
+from theatre.views import ManageUserView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("register/", CreateUserView.as_view(), name="create"),
-    path("login/", CreateTokenView.as_view(), name="token"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("me/", ManageUserView.as_view(), name="manage"),
     path(
         "api/", include("theatre_service.urls", namespace="theatre-api")
