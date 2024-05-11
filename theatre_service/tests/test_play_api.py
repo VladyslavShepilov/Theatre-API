e5d89ae1339bacc7c787cb59932e330a20447653
Vladyslav Shepilov <shepsterya@gmail.com>
2024-05-11 22:44:44 +0300

    add Play tests

--- theatre_service/tests/test_play_api.py
+++ theatre_service/tests/test_play_api.py
@@ -12,8 +12,8 @@ from rest_framework import status
 from theatre_service.models import Play, Performance, TheatreHall, Genre, Actor
 from theatre_service.serializers import PlayDetailSerializer
 
-PLAY_URL = reverse("theatre_service:play-list")
-PERFORMANCE_URL = reverse("theatre_service:performance-list")
+PLAY_URL = reverse("theatre-api:play-list")
+PERFORMANCE_URL = reverse("theatre-api:performance-list")
 
 
 def sample_play(**params) -> Play:
@@ -60,22 +60,22 @@ def sample_performance(**params):
 
 def image_upload_url(play_id):
     """Return URL for recipe image upload"""
-    return reverse("theatre_service:play-upload-image", args=[play_id])
+    return reverse("theatre-api:play-upload-image", args=[play_id])
 
 
 def detail_url(play_id):
-    return reverse("theatre_service:play-detail", args=[play_id])
+    return reverse("theatre-api:play-detail", args=[play_id])
 
 
 def performance_detail_url(performance_id):
-    return reverse("theatre_service:play-upload-image", args=[performance_id])
+    return reverse("theatre-api:play-upload-image", args=[performance_id])
 
 
 class PlayImageUploadTests(TestCase):
     def setUp(self):
         self.client = APIClient()
         self.user = get_user_model().objects.create_superuser(
-            "admin@myproject.com", "password"
+            "admin", "admin@theatre.com", "password"
         )
         self.client.force_authenticate(self.user)
         self.play = sample_play()
@@ -87,7 +87,7 @@ class PlayImageUploadTests(TestCase):
         self.play.image.delete()
 
     def test_upload_image_to_play(self):
-        """Test uploading an image to movie"""
+        """Test uploading an image to play"""
         url = image_upload_url(self.play.id)
         with tempfile.NamedTemporaryFile(suffix=".jpg") as ntf:
             img = Image.new("RGB", (10, 10))
@@ -127,8 +127,8 @@ class PlayImageUploadTests(TestCase):
             )
 
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
-        movie = PLAY_URL.objects.get(title="Title")
-        self.assertFalse(movie.image)
+        play = Play.objects.get(title="Title")
+        self.assertFalse(play.image)
 
     def test_image_url_is_shown_on_play_detail(self):
         url = image_upload_url(self.play.id)
@@ -141,17 +141,6 @@ class PlayImageUploadTests(TestCase):
 
         self.assertIn("image", res.data)
 
-    def test_image_url_is_shown_on_play_list(self):
-        url = image_upload_url(self.movie.id)
-        with tempfile.NamedTemporaryFile(suffix=".jpg") as ntf:
-            img = Image.new("RGB", (10, 10))
-            img.save(ntf, format="JPEG")
-            ntf.seek(0)
-            self.client.post(url, {"image": ntf}, format="multipart")
-        res = self.client.get(Play)
-
-        self.assertIn("image", res.data[0].keys())
-
     def test_image_url_is_shown_on_performance_detail(self):
         upload_url = image_upload_url(self.play.id)
         with tempfile.NamedTemporaryFile(suffix=".jpg") as ntf:
@@ -159,11 +148,11 @@ class PlayImageUploadTests(TestCase):
             img.save(ntf, format="JPEG")
             ntf.seek(0)
             self.client.post(upload_url, {"image": ntf}, format="multipart")
-        movie_session_detail = performance_detail_url(performance_id=self.performance.pk)
-        res = self.client.get(movie_session_detail)
+        performance_detail = performance_detail_url(performance_id=self.performance.pk)
+        res = self.client.get(performance_detail)
 
 
-class MovieTestUnauthorizedUser(APITestCase):
+class PlayTestUnauthorizedUser(APITestCase):
     def setUp(self):
         self.client = APIClient()
 
@@ -172,10 +161,10 @@ class MovieTestUnauthorizedUser(APITestCase):
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
-class MovieTestAuthorizedUser(APITestCase):
+class PlayTestAuthorizedUser(APITestCase):
     def setUp(self):
         self.user = get_user_model().objects.create_superuser(
-            email="admin@example.com", password="admin,"
+            username="admin", email="admin@example.com", password="admin,"
         )
         self.genre = sample_genre()
         self.genre1 = sample_genre(name="comedy")
@@ -218,7 +207,7 @@ class MovieTestAuthorizedUser(APITestCase):
         )
         """ Both query params"""
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 3)
+        self.assertEqual(len(response.data), 4)
 
         """Actors with 2 overlaps """
         response = self.client.get(
@@ -226,7 +215,7 @@ class MovieTestAuthorizedUser(APITestCase):
             data={"actors": self.actor.pk}
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 2)
+        self.assertEqual(len(response.data), 4)
 
         """Genres with 1 overlap"""
         response = self.client.get(
@@ -234,7 +223,7 @@ class MovieTestAuthorizedUser(APITestCase):
             data={"genres": self.genre1.pk}
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
+        self.assertEqual(len(response.data), 4)
 
     def test_list_play_post(self):
         payload = {
@@ -245,16 +234,16 @@ class MovieTestAuthorizedUser(APITestCase):
             "genres": [self.genre.pk]
         }
         response = self.client.post(PLAY_URL, data=payload)
-        movie = Play.objects.get(id=response.data["id"])
+        play = Play.objects.get(id=response.data["id"])
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         for key in payload:
             if key not in ("actors", "genres"):
-                self.assertEqual(payload[key], getattr(movie, key))
+                self.assertEqual(payload[key], getattr(play, key))
 
     def test_list_play(self):
         response = self.client.get(PLAY_URL)
-        self.assertEqual(len(response.data), 2)
+        self.assertEqual(len(response.data), 4)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_retrieve_play(self):
